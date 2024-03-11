628303d80c48f7e2c07d3ef26f42232e9220a688
t-yamamichi <t-yamamichi@gleamorb.co.jp>
2024-03-11 15:24:14 +0900

    bootstrapサンプル追加 （ぐちゃぐちゃ追加したからいらないのあるかも）

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root to: 'home#index'
+  
+  get 'home/index'
+  get 'home/list'
   namespace :admin do
     resources :contacts
   end
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,15 @@
 const { environment } = require('@rails/webpacker')
 
+//ここから
+var webpack = require('webpack');
+environment.plugins.prepend(
+   'Provide',
+   new webpack.ProvidePlugin({
+       $: 'jquery/src/jquery',
+       jQuery: 'jquery/src/jquery',
+       Popper: ['popper.js', 'default']
+   })
+)
+//ここまでを追加
+
 module.exports = environment
